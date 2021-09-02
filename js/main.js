1049688da0dd6f0b0a143b30b9368898947d23f3
Saiful Shuvo <saif.shuvo@hotmail.com>
2021-09-02 12:09:21 +0600

    Update

--- js/main.js
+++ js/main.js
@@ -5,26 +5,32 @@ const resultFound = document.getElementById("result-found");
 // Capture ddata
 const captureInputValues = () => {
     const search =  searchInput.value;
-    const searchURL = `http://openlibrary.org/search.json?q=${search}`;
+    const searchURL = `https://openlibrary.org/search.json?q=${search}`;
     fetch(searchURL)
     .then(res => res.json())
-    .then(data => dataStorage(data.docs))
+    .then(data => dataStorage(data.docs));
 };
 // store data in this function
 // Loop through data
-function dataStorage(data){
+
+{/*   */}
+const dataStorage = (data) => {
     data.forEach(book => {
         // console.log(book);
-        resultFound.innerText = ``;
+        resultFound.innerHTML = `<p>${data.length} data found</p>`;
         const bookDiv = document.createElement("div");
-        bookDiv.classList.add("col-md-3" , "col-12");
+        bookDiv.classList.add("col-md-3" , "col-12" , "item");
         bookDiv.innerHTML = `
-            <h3 id="book-title" class="pt-3">${book.title}</h3>
-            <p id="book-author">Author: ${book.author_name}</p>
-            <p id="book-published">Published date: ${book.publish_date}</p>
+            <div>
+                <h3 class="pt-3">${book.title}</h3>
+                <p>Author: ${book.author_name}</p>
+                <p>Publisher: ${book.publisher}</p>
+                <p>Published date: ${book.publish_date}</p> 
+            </div>
         `;
         bookContainer.append(bookDiv);
-    });
-};
+        })
+}
+
 //Event listener
 searchBtn.addEventListener("click", captureInputValues);
