f302aa77c823e6e0826e722595dff5956c2a3949
Saiful Shuvo <saif.shuvo@hotmail.com>
2021-09-02 17:50:53 +0600

    Final update

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ const searchBtn = document.getElementById("search-btn");
 const bookContainer = document.getElementById("book-container");
 const resultFound = document.getElementById("result-found");
 const errorDiv = document.getElementById("error-div");
-const bookCover = document.getElementById("book-cover");
 
 /*
 **  Function name: emptyContent  
@@ -23,6 +22,8 @@ const toggleSpinner = displayStyle => {
     const spinner = document.getElementById("spinner");
     spinner.style.display = displayStyle;
 };
+
+
 /* 
 **  Function name: captureInputValues 
 **  Load and capture data from API to display
@@ -38,46 +39,55 @@ const captureInputValues = () => {
         return;
     } else {
         emptyContent();
+        // display spinner while loading..
         toggleSpinner("block");
         const searchURL = `https://openlibrary.org/search.json?q=${search}`;
         fetch(searchURL)
         .then(res => res.json())
-        .then(data => dataStorage(data.docs));
+        .then(data => displayData(data.docs));
     }
 };
 
 /*  
-**  Function name: dataStorage
+**  Function name: displayData
 **  store data in this function
 **  Loop through data
 */
 
-const dataStorage = (data) => { 
-    if (data.length === 0) {
-        // console.log("No data found");
+const displayData = (data) => { 
+    if (data.length !== 0) {
+        // enter into this block if input field has value
+        errorDiv.innerText = "";
+        // Loop through data
+        data.forEach(book => {
+            // console.log(book.cover_i);
+            resultFound.innerHTML = `<p>${data.length} books found</p>`;
+            const bookDiv = document.createElement("div");
+            bookDiv.classList.add("col-md-3" , "col-12" , "item");
+            bookDiv.innerHTML = `
+                <div>
+                    <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="img-fluid" alt="Book cover image">
+                    <h3 class="pt-3">${book.title}</h3>
+                    <p>Author: ${book.author_name}</p>
+                    <p>Publisher: ${book.publisher}</p>
+                    <p>Published date: ${book.publish_date}</p> 
+                </div>
+            `;
+            bookContainer.append(bookDiv);
+            });
+    } else {
+        //display error if no data found
         errorDiv.innerHTML = `
             <p style="display: inline; color: red;">No such book name found!</p>
         `;
-    } else {
-        errorDiv.innerText = "";
     }
-    data.forEach(book => {
-        // console.log(book.cover_i);
-        resultFound.innerHTML = `<p>${data.length} books found</p>`;
-        const bookDiv = document.createElement("div");
-        bookDiv.classList.add("col-md-3" , "col-12" , "item");
-        bookDiv.innerHTML = `
-            <div>
-                <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="img-fluid" alt="Book cover image">
-                <h3 class="pt-3">${book.title}</h3>
-                <p>Author: ${book.author_name}</p>
-                <p>Publisher: ${book.publisher}</p>
-                <p>Published date: ${book.publish_date}</p> 
-            </div>
-        `;
-        bookContainer.append(bookDiv);
-        });
-        toggleSpinner("none");
+    // Stop spinner
+    toggleSpinner("none");
 }
-// Event listener: execute after clicking search button
+
+/*
+**  Event listener: execute after clicking search button
+**  Placed here beacause of the console error:
+**  Uncaught ReferenceError: Cannot access 'captureInputValues' before initialization 
+*/
 searchBtn.addEventListener("click", captureInputValues);
