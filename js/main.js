df9e5979bd8fb27ec4e3f88f65b1cfb5d9d363bb
Saiful Shuvo <saif.shuvo@hotmail.com>
2021-09-02 14:37:58 +0600

    Update without book cover

--- js/main.js
+++ js/main.js
@@ -2,19 +2,43 @@ const searchInput = document.getElementById("search-input");
 const searchBtn = document.getElementById("search-btn");
 const bookContainer = document.getElementById("book-container");
 const resultFound = document.getElementById("result-found");
+const errorDiv = document.getElementById("error-div");
+
+//empty content
+const emptyContent = () =>{
+    searchInput.value = "";
+    bookContainer.innerHTML = "";
+    // countryDetails.innerHTML = "";
+}
+
 // Capture ddata
 const captureInputValues = () => {
     const search =  searchInput.value;
-    const searchURL = `https://openlibrary.org/search.json?q=${search}`;
-    fetch(searchURL)
-    .then(res => res.json())
-    .then(data => dataStorage(data.docs));
+    if (search === "") {
+        emptyContent();
+        errorDiv.innerHTML = `
+        <p style="color: red;">Search field cannot be empty!</p>
+        `;
+        return;
+    } else {
+        emptyContent();
+        const searchURL = `https://openlibrary.org/search.json?q=${search}`;
+        fetch(searchURL)
+        .then(res => res.json())
+        .then(data => dataStorage(data.docs));
+    }
 };
 // store data in this function
 // Loop through data
-
-{/*   */}
-const dataStorage = (data) => {
+const dataStorage = (data) => { 
+    if (data.length === 0) {
+        // console.log("No data found");
+        errorDiv.innerHTML = `
+            <p style="display: inline; color: red;">No such book name found!</p>
+        `;
+    } else {
+        errorDiv.innerText = "";
+    }
     data.forEach(book => {
         // console.log(book);
         resultFound.innerHTML = `<p>${data.length} data found</p>`;
@@ -34,3 +58,4 @@ const dataStorage = (data) => {
 
 //Event listener
 searchBtn.addEventListener("click", captureInputValues);
+
